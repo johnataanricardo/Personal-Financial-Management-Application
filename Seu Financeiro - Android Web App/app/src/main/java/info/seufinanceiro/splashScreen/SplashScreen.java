374d0273979676328d809de12a45930a43cc7bb9
Johnatan Ricardo Martins <johnataanmartins@gmail.com>
2018-08-31 18:26:57 -0300

    Created the Android Application Web App.

---
+++ Seu Financeiro - Android Web App/app/src/main/java/info/seufinanceiro/splashScreen/SplashScreen.java
@@ -0,0 +1,32 @@
+package info.seufinanceiro.splashScreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import info.seufinanceiro.R;
+import info.seufinanceiro.main.WebApp;
+
+public class SplashScreen extends Activity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, WebApp.class);
+                startActivity(i);
+
+                finish();
+            }
+
+        }, SPLASH_TIME_OUT);
+    }
+
+}
