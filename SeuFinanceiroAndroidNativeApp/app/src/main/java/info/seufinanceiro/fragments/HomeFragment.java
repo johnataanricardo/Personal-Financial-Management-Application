156e1f11709b561c7969aa5c83460594494203dc
Alana Domit Bittar <alana.d.b@tutanota.com>
2018-10-21 11:22:00 -0300

    home dialogs

--- SeuFinanceiroAndroidNativeApp/app/src/main/java/info/seufinanceiro/fragments/HomeFragment.java
+++ SeuFinanceiroAndroidNativeApp/app/src/main/java/info/seufinanceiro/fragments/HomeFragment.java
@@ -1,23 +1,40 @@
 package info.seufinanceiro.fragments;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import info.seufinanceiro.R;
 
 public class HomeFragment extends Fragment {
 
     private ContentTab listener;
+    private Context mContext;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_layout, null);
+        final View view = inflater.inflate(R.layout.home_layout, null);
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = onCreateDialogSingleChoice();
+                dialog.show();
+            }
+        });
+        return view;
     }
 
     @Override
@@ -33,11 +50,68 @@ public class HomeFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        mContext = context;
         if (context instanceof ContentTab) {
             listener = (ContentTab) context;
         } else {
             throw new ClassCastException();
         }
+    }
+
+    public Dialog onCreateDialogSingleChoice() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        CharSequence[] array = {"Entrada", "Saída"};
+        final EditText input = new EditText(mContext);
+        input.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+
+        builder.setTitle("Escolha o tipo de transação e insira o valor")
+
+                .setView(input)
+
+                .setSingleChoiceItems(array, 1, new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+
+                .setPositiveButton("Salvar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Dialog pickerDialog = onCreateDialogPick();
+                        dialog.dismiss();
+                        pickerDialog.show();
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        return builder.create();
+    }
+
+    public Dialog onCreateDialogPick() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        CharSequence[] array = {"Alimentação", "Higiene", "Pet", "Roupas", "Salário"};
+
+        builder.setTitle("Escolha a categoria")
+                .setItems(array, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // The 'which' argument contains the index position
+                        // of the selected item
+                    }
+                })
+
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        return builder.create();
     }
 
     public interface ContentTab {
--- SeuFinanceiroAndroidNativeApp/app/src/main/java/info/seufinanceiro/main/MainActivity.java
+++ SeuFinanceiroAndroidNativeApp/app/src/main/java/info/seufinanceiro/main/MainActivity.java
@@ -44,14 +44,6 @@ public class MainActivity extends AppCompatActivity
         fragment.setArguments(bundle);
         setFragment(fragment);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
 
         tabLayout = findViewById(R.id.month_tab);
         setTabLayoutListener(tabLayout);
